2c0d50ff6dad17f489dcdbfdbcad8b367a2621cf
test <test@test.com>
2025-12-26 20:32:28 +0000

    fixing deployment

--- src/main/java/com/example/TimeController.java
+++ src/main/java/com/example/TimeController.java
@@ -6,14 +6,19 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
-@Controller("/time")
+@Controller
 public class TimeController {
 
-    @Get
+    @Get("/time")
     public Map<String, String> index() {
         return Map.of(
                 "now", ZonedDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME),
                 "service", "micronaut-time-provider"
         );
+    }
+
+    @Get("/health")
+    public Map<String, String> health() {
+        return Map.of("status", "UP");
     }
 }
